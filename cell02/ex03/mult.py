1112cc609bd5e8c59fb37835cc3df8329cc69991
ksaelim42 <131423859+ksaelim42@users.noreply.github.com>
2026-02-11 07:54:24 +0000

    cell2.3

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-password = "Python is awesome"
-
-if input() == password:
-    print("ACCESS GRANTED")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+result = num1 * num2
+print(num1, "x", num2, "=", result)
+if result < 0:
+    print("This result is negative.")
+elif result == 0:
+    print("This result is positive and negative.")
 else:
-    print("ACCESS DENIED")
+    print("This result is positive.")
