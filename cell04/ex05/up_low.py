e031272e053dce9b18b19139b663feba50fcbb55
ksaelim42 <131423859+ksaelim42@users.noreply.github.com>
2026-02-12 11:20:17 +0000

    cell04

--- cell04/ex05/up_low.py
+++ cell04/ex05/up_low.py
@@ -4,9 +4,9 @@ try:
     word = input("")
     for char in word:
         if char.islower():
-            print(char.upper())
+            print(char.upper(), end="")
         elif char.isupper():
-            print(char.lower())
+            print(char.lower(), end="")
             
 except:
     print("Error")
